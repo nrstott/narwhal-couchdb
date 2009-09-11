8cc809b52e80f17f551ffae4c98eb3726551e090
Nathan <nathan@Planck.(none)>
2009-09-10 22:18:32 -0400

    removed load of jQuery from tests file

--- tests/couchdb.tests.js
+++ tests/couchdb.tests.js
@@ -2,14 +2,6 @@ version(170);
 
 var fs = require("file");
 
-if (typeof $ == "undefined") {
-    var path = fs.path(module.path).dirname();
-    print("FIRST REQUEST");
-    load(path + "/../lib/env.rhino.js");
-    window.location = path + "/../lib/test.html";
-    load(path + "/../lib/jquery-1.3.2.js");
-}
-
 exports.testCouchDb = require("./couchdb/all-tests");
 
 if (require.main == module.id)
